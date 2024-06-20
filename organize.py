a50be4bc2e9f0fe1346c0c42ebeb51ea051e02dc
neetikaaryal <neetikaaryal16@gmail.com>
2024-06-20 17:15:16 +0545

    organize files in seperate folders

---
+++ organize.py
@@ -0,0 +1,28 @@
+import os
+
+folder_path = "/home/dpaisa/Downloads/"
+# print(os.listdir(folder_path))
+folders = {
+    "image" : ["img", "png", "jpg", "jpeg"],
+    "deb" : ["deb"],
+}
+def assign_folder(extension:str)->str | bool:
+    for folder_name in folders:
+        # print(folder_name)
+        folder_ext = folders[folder_name]
+        # print(folder_ext)
+        if extension in folder_ext:
+            return folder_name
+        else:
+            return False    
+        
+folder_name = assign_folder("pdf")
+print(folder_name)
+# for i in os.listdir(folder_path):
+#     # print(i)
+#     if "." in i:
+#         # print("file")
+#         extention = i.split(".")[1]
+#         print(extention)
+#     else:
+#         print("folder")
